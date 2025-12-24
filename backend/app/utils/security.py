2372b6599a4a9914c85145b93710cf587a4963e1
İrem Teker <147082141+iremteker@users.noreply.github.com>
2025-12-24 16:17:51 +0300

    feat: implement token creation and decoding functions

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,6 +1,16 @@
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = " "
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")
@@ -10,3 +20,26 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_minutes: Optional[int] = None) -> str:
+    """
+    data: token içine koyacağımız bilgiler (örn: {"sub": "1"})
+    """
+
+    to_encode = data.copy()
+    minutes = expires_minutes if expires_minutes is not None else ACCESS_TOKEN_EXPIRE_MINUTES
+    expire = datetime.now(timezone.utc) + timedelta(minutes=minutes)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_token(token: str) -> dict:
+    """
+    Token geçersizse {} döner.
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return {}
