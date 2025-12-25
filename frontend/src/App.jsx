697bc1af8ef194034d2534d6c58d2fac94e3e6a8
İrem Teker <147082141+iremteker@users.noreply.github.com>
2025-12-26 01:47:13 +0300

    feat: frontend login, backend ile entegre edildi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,66 @@
+import { useState } from "react";
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  console.log("Sending:", {
+  email: email,
+  password: password,
+});
+
+
+  try {
+    const response = await fetch("http://127.0.0.1:8000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        username: email,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log("Backend response:", data);
+  } catch (error) {
+    console.error("Login error:", error);
+  }
+};
+
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Kitap & Film Platformu</h1>
-      <p>Frontend hazır 🚀</p>
+    <div style={{ padding: "2rem", maxWidth: "400px" }}>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: "1rem" }}>
+          <label>Email</label><br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ width: "100%" }}
+          />
+        </div>
+
+        <div style={{ marginBottom: "1rem" }}>
+          <label>Password</label><br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ width: "100%" }}
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
